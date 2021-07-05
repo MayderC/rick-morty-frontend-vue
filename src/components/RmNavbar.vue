1fbf58e0c276c5fa29b9f5c0c08e18f09c5cdd67
MayderC <maydercortes1@gmail.com>
2021-07-04 19:02:57 -0600

    add menu-responsive, html, css

--- src/components/RmNavbar.vue
+++ src/components/RmNavbar.vue
@@ -15,6 +15,8 @@ nav.navbar
     li.navbar__item
       router-link(to="/episode" class="navbar__link")
         p Episode
+  .menu__btn
+    .menu__btn-bar
 </template>
 
 <script>
@@ -24,6 +26,8 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+
+
 
 .img-container
   height: 90px
@@ -41,11 +45,46 @@ img:hover
 
 
 .navbar
+  position: relative
   width: 100%
   background-color: #262742
   display: flex
   justify-content: space-around
   box-shadow: 0px 10px 40px #15121b
+
+.menu__btn
+  height: 50px
+  width: 50px
+  position: absolute
+  top: 20px
+  right: 20px
+
+.menu__btn-bar, .menu__btn-bar:before, .menu__btn-bar:after
+  position: absolute
+  content: ""
+  background: white
+  border-radius: 10px
+  display: none
+
+.menu__btn-bar
+  height: 5px
+  width: 50px
+  top: calc(50% - 4px)
+
+.menu__btn-bar:before
+  height: 5px
+  width: 50px
+  top: 15px
+
+.menu__btn-bar:after
+  position: absolute
+  content: ""
+  background: white
+  height: 5px
+  width: 50px
+  top: -15px
+
+
 
 .navbar__list
   padding: 0
@@ -74,4 +113,25 @@ img:hover
 .navbar__link:hover
   text-shadow: 0px 0px 40px #08b2c9
   transform: scale(1.1)
+
+.navbar__list
+  height: 90px
+
+  .navbar__item
+    height: 90px
+
+
+@media (max-width: 650px)
+  .navbar
+    display: block
+  .navbar__list
+    display: none
+  .menu--responsive
+    display: block
+    height: 300px
+    margin: auto
+
+
+  .menu__btn-bar, .menu__btn-bar:before, .menu__btn-bar:after
+    display: block
 </style>
